db01731306a4bba1307067c762445de96f0cea05
Israel Omitiran <omitiranisrael@gmail.com>
2022-09-05 20:40:30 +0100

    done with navbar

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -53,27 +53,27 @@ const Navbar = () => {
                 <div className={toggle ? 'active' : 'menu-list'}>
                     <ul>
                         <li >
-                            <Link onClick={() => setToggle(prevToggle => !prevToggle)} to='home'  smooth={true} duration={1200}>
+                            <Link onClick={() => setToggle(prevToggle => !prevToggle)} to='home' offset={-70}  smooth={true} duration={1200}>
                                 Home
                             </Link>
                         </li>
                         <li>
-                            <Link onClick={() => setToggle(prevToggle => !prevToggle)} to='about'  smooth={true} duration={1200}>
+                            <Link onClick={() => setToggle(prevToggle => !prevToggle)} to='about' offset={-70}  smooth={true} duration={1200}>
                                 About Me
                             </Link>
                         </li>
                         <li>
-                            <Link onClick={() => setToggle(prevToggle => !prevToggle)} to='project'  smooth={true} duration={1200}>
+                            <Link onClick={() => setToggle(prevToggle => !prevToggle)} to='project' offset={-70}  smooth={true} duration={1200}>
                                 Project
                             </Link>
                         </li>
                         <li>
-                            <Link onClick={() => setToggle(prevToggle => !prevToggle)} to='service'  smooth={true} duration={1200}>
+                            <Link onClick={() => setToggle(prevToggle => !prevToggle)} to='service' offset={-70} smooth={true} duration={1200}>
                                 Services
                             </Link>
                         </li>
                         <li>
-                        <Link onClick={() => setToggle(prevToggle => !prevToggle)} to='contact'  smooth={true} duration={1200}>
+                        <Link onClick={() => setToggle(prevToggle => !prevToggle)} to='contact' offset={-70} smooth={true} duration={1200}>
                                 Contact
                             </Link>
                         </li>
